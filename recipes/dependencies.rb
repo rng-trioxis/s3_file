aab5ab5cf16e1780ba21e2fcb329a5ebeaacefde
Ray Ng <rng@frgteam.com>
2015-03-30 17:53:28 +1100

    Removed reset-client

--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -1,8 +1,10 @@
-require 'rest-client'
+
 
 chef_gem "rest-client" do
   action :install
   version "1.7.3"
 end
+
+require 'rest-client'
 
 RestClient::Request.execute(:url => 'https://s3-ap-southeast-2.amazonaws.com', :method => :get, :verify_ssl => false)
